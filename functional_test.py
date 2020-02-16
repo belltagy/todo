1b252d61029fcd76676b14198ca0a7b9fce0cca1
unknown <mmk.mmk0@gmail.com>
2020-02-16 21:44:53 +0200

    refactoring home page view to use a template

--- functional_test.py
+++ functional_test.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -14,7 +14,7 @@ class NewVistorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
-# to check out its homepage
+        # to check out its homepage
             self.browser.get('http://localhost:8000')
             # She notices the page title and header mention to-do lists
             self.assertIn('To-Do', self.browser.title)
