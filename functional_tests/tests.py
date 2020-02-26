167725fdab2ed8d4bc5f82718d4a971c0ec8ebdb
unknown <mmk.mmk0@gmail.com>
2020-02-26 21:50:46 +0200

    firdt step for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -6,7 +6,7 @@ from django.test import LiveServerTestCase
 class NewVistorTest(LiveServerTestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome()
+        self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
@@ -16,33 +16,33 @@ class NewVistorTest(LiveServerTestCase):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
            # self.browser.get('http://localhost:8000')
-            self.browser.get(self.live_server_url)
-            # She notices the page title and header mention to-do lists
-            self.assertIn('To-Do', self.browser.title)
-            header_text = self.browser.find_element_by_tag_name('h1').text
-            self.assertIn('To-Do', header_text)
-            # She is invited to enter a to-do item straight away
-            inputbox = self.browser.find_element_by_id('id_new_item')
-            self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-            )
-            # She types "Buy peacock feathers" into a text box (Edith's hobby
-            # is tying fly-fishing lures)
-            inputbox.send_keys('Buy peacock feathers')
-            # When she hits enter, the page updates, and now the page lists
-            # "1: Buy peacock feathers" as an item in a to-do list table
-            inputbox.send_keys(Keys.ENTER)
-            edith_list_url = self.browser.current_url
-            self.assertRegex(edith_list_url,'/lists/.+')
-            self.check_for_row_in_list_table('1: Buy peacock feathers')
+            # self.browser.get(self.live_server_url)
+            # # She notices the page title and header mention to-do lists
+            # self.assertIn('To-Do', self.browser.title)
+            # header_text = self.browser.find_element_by_tag_name('h1').text
+            # self.assertIn('To-Do', header_text)
+            # # She is invited to enter a to-do item straight away
+            # inputbox = self.browser.find_element_by_id('id_new_item')
+            # self.assertEqual(
+            # inputbox.get_attribute('placeholder'),
+            # 'Enter a to-do item'
+            # )
+            # # She types "Buy peacock feathers" into a text box (Edith's hobby
+            # # is tying fly-fishing lures)
+            # inputbox.send_keys('Buy peacock feathers')
+            # # When she hits enter, the page updates, and now the page lists
+            # # "1: Buy peacock feathers" as an item in a to-do list table
+            # inputbox.send_keys(Keys.ENTER)
+            # edith_list_url = self.browser.current_url
+            # self.assertRegex(edith_list_url,'/lists/.+')
+            # self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-            self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-            self.check_for_row_in_list_table('1: Buy peacock feathers')
-            # Now a new user, Francis, comes along to the site.
-            ## We use a new browser session to make sure that no information
-            ## of Edith's is coming through from cookies etc #
-            self.browser.quit()
+            # self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+            # self.check_for_row_in_list_table('1: Buy peacock feathers')
+            # # Now a new user, Francis, comes along to the site.
+            # ## We use a new browser session to make sure that no information
+            # ## of Edith's is coming through from cookies etc #
+            # self.browser.quit()
             self.browser = webdriver.Chrome()
             # Francis visits the home page. There is no sign of Edith's
             # list
@@ -69,6 +69,21 @@ class NewVistorTest(LiveServerTestCase):
             table = self.browser.find_element_by_id('id_list_table')
             rows = table.find_elements_by_tag_name('tr') 
             self.assertIn(row_text, [row.text for row in rows])                    
+
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+        #she notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+
+        inputbox.send_keys("test style\n")
+
+        self.assertAlmostEqual(
+            inputbox.location['x']+inputbox.size['width']/2,512,delta=10)
+
+
+
 
 if __name__ == '__main__': #
     unittest.main(warnings='ignore') #
