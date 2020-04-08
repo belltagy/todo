49f7b33ff3294ca7cb50037586144085920ffe5a
unknown <mmk.mmk0@gmail.com>
2020-04-09 00:04:07 +0200

    pushing to remote repository

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,17 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import sys
 #from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 class NewVistorTest(StaticLiveServerTestCase):
-
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
-
-    def tearDown(self):
-        self.browser.refresh()
-        self.browser.quit()
+    @classmethod
+    def setUpClass(cls): #
+        for arg in sys.argv: #
+            if 'liveserver' in arg: #
+            cls.server_url = 'http://' + arg.split('=')[1] #
+            return #
+    super().setUpClass()
+    cls.server_url = cls.live_server_url
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+        super().tearDownClass()
+        
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -47,7 +53,7 @@ class NewVistorTest(StaticLiveServerTestCase):
             self.browser = webdriver.Chrome()
             # Francis visits the home page. There is no sign of Edith's
             # list
-            self.browser.get(self.live_server_url)
+            self.browser.get(self.server_url)
             page_text = self.browser.find_element_by_tag_name('body').text
             self.assertNotIn('Buy peacock feathers', page_text)
             self.assertNotIn('make a fly', page_text)
